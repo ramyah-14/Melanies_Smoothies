c88255eb73fe4ca59a4c3601dd612efd69426f15
ramyah-14 <ramyanandak@gmail.com>
2026-06-22 15:07:04 +0530

    Parse JSON response from Smoothiefroot API
    API Exercise 1_1

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,7 +5,7 @@ from snowflake.snowpark.functions import col
 
 import requests  
 smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-st.text(smoothiefroot_response)
+st.text(smoothiefroot_response.json())
 
 # Write directly to the app
 st.title(f"Cup With Straw:Customize your smoothie")
